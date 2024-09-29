7373e9fce39b1511f11266fdf2a8eb5f200d21b6
AngelSaad0 <engysaad573new@gmail.com>
2024-09-29 22:02:40 +0300

    Initial Commit

---
+++ GameApp/GameAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  GameAppApp.swift
+//  GameApp
+//
+//  Created by Engy on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GameAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ GameAppTests/GameAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameAppTests.swift
+//  GameAppTests
+//
+//  Created by Engy on 9/29/24.
+//
+
+import Testing
+@testable import GameApp
+
+struct GameAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
